a781a5e3514c4ca9559368ad184473c5d5fdccec
AbdullahAlKhafajiDev <abdullahalkafajy321@gmail.com>
2023-10-09 12:29:17 -0500

    configured TailwindCSS file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'phone': {'min': '250px', 'max': '469px'},
+      'tablet': {'min': '470px', 'max': '1023px'},
+      'desktop': '1024px'
+    },
     extend: {},
   },
   plugins: [],
