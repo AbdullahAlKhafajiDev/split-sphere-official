790f39e2574a09787cd05ac09a76f6f00cd3eea8
AbdullahAlKhafajiDev <abdullahalkafajy321@gmail.com>
2024-07-22 21:47:42 -0500

    Ensured mobile operability for landing component.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import WelcomeScreen from './components/WelcomeScreen.js';
 
 function App() {
   return (
-    <div className='bg-[#0F0D1A] pt-[3rem] pb-[4rem]'>
       <WelcomeScreen />
-    </div>
     
   );
 }
